327e8bfdb6906a3680e8782bcccf1285e5edce45
eldorado-22 <jumashevv992@gmail.com>
2023-11-18 03:27:52 +0600

    eccomerce-ui

---
+++ src/views/Category/AddCategory.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="text-center">
+                <h3>Add Category</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6">
+                <form>
+                <div class="form-group">
+                    <label>Category Name</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Category Description</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Category Image</label>
+                    <input type="text" class="form-control">
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+            </div>
+            <div class="col-3"></div>
+
+        </div>
+    </div>
+</template>
+
+<script >
+export default {
+    setup() {
+
+    },
+}
+</script>
+
+<style>
+</style>
